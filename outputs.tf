96c3fdd9cda81d43b92d82f3d7366ebb5dfc5350
SisiLucyLiu <112094327+SisiLucyLiu@users.noreply.github.com>
2022-08-25 16:23:31 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,4 @@ output "website_endpoint" {
   description = "HTTP endpoint for example website."
   value       = module.s3-webapp.endpoint
 }
-output "website_endpoint" {
-  value = module.s3-webapp.endpoint
-}
 
