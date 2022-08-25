68a7aabb147b31b5b163350b39678c1907e5edae
SisiLucyLiu <112094327+SisiLucyLiu@users.noreply.github.com>
2022-08-25 16:22:17 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,15 @@ output "website_endpoint" {
   description = "HTTP endpoint for example website."
   value       = module.s3-webapp.endpoint
 }
+  variable "region" {
+  description = "This is the cloud hosting region where your webapp will be deployed."
+}
+
+variable "prefix" {
+  description = "This is the environment your webapp will be prefixed with. dev, qa, or prod"
+}
+
+variable "name" {
+  description = "Your name to attach to the webapp address"
+}
+
